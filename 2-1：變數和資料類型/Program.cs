13c987fa08f72a822e87b813c8a180b7ba3b433e
Ivan Liu <j205073@gmail.com>
2023-08-13 07:40:22 +0800

    2-1 for jhon

--- 2-1：變數和資料類型/Program.cs
+++ 2-1：變數和資料類型/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            dynamic test = 1;
+            dynamic test = 2;
             Console.WriteLine("Hello, World!");
         }
     }
