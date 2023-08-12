fc4b92ca51501244ed6cd7c7b53da137191ec32a
ivaninfozh <ivan.info.zh@gmail.com>
2023-08-13 07:01:19 +0800

    try commit

---
+++ 2-1：變數和資料類型/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _2_1_變數和資料類型
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic test = 1;
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+
+    public class Test : ReadOnly範例
+    {
+        readonly int MyReadOnlyVar = 10;
+
+        public Test(int value) : base(value)
+        {
+            MyReadOnlyVar = value; // 在構造函數內可以設置
+            ReadOnly範例.StaticFunc();
+        }
+        public override void MyReadOnly()
+        {
+        }
+    }
+    public abstract class ReadOnly範例
+    {
+        readonly int MyReadOnlyVar = 10;
+
+        public ReadOnly範例(int value)
+        {
+            MyReadOnlyVar = value; // 在構造函數內可以設置
+        }
+
+        public virtual void MyReadOnly() { }
+
+        public static void StaticFunc()
+        { 
+        }
+
+
+    }
+}
